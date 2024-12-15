c6a90a2e1adbf262c5deb2763705b8e4ad01d6c9
Николай <i@nik-levchenko.ru>
2024-12-15 14:22:04 +0700

    Адаптированы локаторы xpath для разных логкализаций

--- xpath/xpath_lesson.py
+++ xpath/xpath_lesson.py
@@ -1,14 +1,14 @@
 wiki_title = "//h1[@id='Добро_пожаловать_в_Википедию,']"
-pdf_download = "//a[@title='Скачать эту страницу как файл PDF']"
-favorite_img = "//div[@id='main-tfa']/child::div[@class='main-box-content']/descendant::img[@class='mw-file-element']"
-enter_comon_text = "//tt[text()='Редактировать просто — что введёшь, то и увидишь.']"
+pdf_download = "//li[@id='coll-download-as-rl']/child::a"#адаптирован
+favorite_img = "//a[@class='mw-file-description'][1]/img[1]"#адаптирован???
+enter_comon_text = "//tbody/tr[4]/td[2]/p/tt"#адаптирован????
 lang_settings = "//button[@class='uls-settings-trigger']"
-login_label = "//label[@for='wpName1']"
-login_field = "//input[@id='wpName1']"
-pass_label = "//label[@for='wpPassword1']"
-pass_field = "//input[@id='wpPassword1']"
-check_box = "//input[@id='wpRemember']"
-enter_button = "//button[@id='wpLoginAttempt']"
-login_help = "//a[text()='Помощь по входу']"
-pass_reset = "//a[@title='Служебная:Сброс пароля']"
-join = "//a[@id='mw-createaccount-join']"
+login_label = "//label[@for='wpName1']"#адаптирован
+login_field = "//input[@id='wpName1']"#адаптирован
+pass_label = "//label[@for='wpPassword1']"#адаптирован
+pass_field = "//input[@id='wpPassword1']"#адаптирован
+check_box = "//input[@id='wpRemember']"#адаптирован
+enter_button = "//button[@id='wpLoginAttempt']"#адаптирован
+login_help = "//div[@class='mw-htmlform-field-HTMLInfoField mw-form-related-link-container mw-userlogin-help cdx-field']/div/a"#адаптирован
+pass_reset = "//div[@class='mw-htmlform-field-HTMLInfoField mw-form-related-link-container cdx-field']/div/a"#адаптирован
+join = "//a[@id='mw-createaccount-join']"#адаптирован
