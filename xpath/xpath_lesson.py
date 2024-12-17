31ade42152f9e9f5d959d514edf72574d6c60123
Николай <i@nik-levchenko.ru>
2024-12-17 10:50:55 +0700

    Исправлены xpath, добавил комменты

--- xpath/xpath_lesson.py
+++ xpath/xpath_lesson.py
@@ -1,16 +1,21 @@
-wiki_title = "//h1[@id='Добро_пожаловать_в_Википедию,']//span"#здесь заголовок википедии, я думаю вряд ли он поменяется
+wiki_title = "//h1[@id='Добро_пожаловать_в_Википедию,']"#здесь заголовок википедии, я думаю вряд ли он поменяется
 #к тому же у него есть id а у всего остального над ним нет. Тогда это норм к нему привязаться?
-pdf_download = "//li[@id='coll-download-as-rl']//a"#адаптирован
-favorite_img = "//div[@id='main-tfa']//div//figure//a//img"#адаптирован???
-enter_comon_text = "//tbody//tr[4]//td[2]//p//tt"#тут ничего не поделаешь, в таблице у тегов никаких атрибутов нет(
-lang_settings = "//nav[@id='p-lang']/button"
-login_label = "//label[@for='wpName1']//span"#адаптирован
-login_field = "//input[@id='wpName1']"#адаптирован
-pass_label = "//label[@for='wpPassword1']//span"#адаптирован
-pass_field = "//input[@id='wpPassword1']"#адаптирован
-check_box = "//input[@id='wpRemember']"#адаптирован
-enter_button = "//button[@id='wpLoginAttempt']"#адаптирован
-login_help = "//div[@class='mw-htmlform-field-HTMLInfoField mw-form-related-link-container mw-userlogin-help cdx-field']//div//a"#здесь тоже,
-# кроме атрибута class у тегов больше ничего нет, и в следующем локаторе также
-pass_reset = "//div[@class='mw-htmlform-field-HTMLInfoField mw-form-related-link-container cdx-field']//div//a"#адаптирован
-join = "//a[@id='mw-createaccount-join']"#адаптирован
+pdf_download = "//*[@id='coll-download-as-rl']//a"#адаптирован
+favorite_img = "//*[@id='main-tfa']//*//figure[contains(@class, 'mw-halign-right')]//a[contains(@class, 'mw-file-description')]//img"
+enter_comon_text = "//*[@id='mw-content-text']//table[contains(@align, 'center')]//tr[4]//td[2]//p/tt"#ближайшая надежная привязка это div с id
+#в таблице кроме align и style тоже ничего нет. А теги в самой таблице вообще без атрибутов
+lang_settings = "//*[@id='p-lang']/button"#короче тогда везде, где есть точный идентификатор, типа id - я ставлю *?
+login_label = "//*[@for='wpName1']//span"#как понимаю for спеииально сделали?
+login_field = "//*[@id='wpName1']"
+pass_label = "//*[@for='wpPassword1']"
+pass_field = "//*[@id='wpPassword1']"
+check_box = "//*[@id='wpRemember']"
+enter_button = "//*[@id='wpLoginAttempt']"
+login_help = "//*[contains(@class, 'mw-userlogin-help')]//a"#То есть смотри - лучше указать часть класса, чем целиком весь?
+#класс целиком - mw-htmlform-field-HTMLInfoField mw-form-related-link-container mw-userlogin-help cdx-field
+#я указываю только как бы логическую для этого элемента часть(помощь с логином - mw-userlogin-help). Потому-что все остальное может измениться. Правильно понял?
+#но в следующем локаторе как раз нет этой логической части в атрибуте
+pass_reset = "//div[@class='mw-htmlform-field-HTMLInfoField mw-form-related-link-container cdx-field']//div//a"#здесь класс почти точно такой же
+#mw-htmlform-field-HTMLInfoField mw-form-related-link-container cdx-field
+#тогда здесь как, целиком только, правильно?
+join = "//*[@id='mw-createaccount-join']"#адаптирован
